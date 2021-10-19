2dedd5ac77acd701a67db8ac51fce625fae6d952
Santana2196 <Santana2196@mail.com>
2021-10-19 19:23:35 -0300

    primeiro commit

---
+++ aula-1/aula1.py
@@ -0,0 +1,21 @@
+NOME = input('Digite o Nome: ')
+IDADE = input('idade: ')
+CPF = input('cpf: ')
+
+print('------------------')
+print(f'Nome: {NOME}')
+print(f'Idade: {IDADE}')
+print(f'Cpf: {CPF}')
+print('------------------')
+
+
+TEXTO = 'Umxpratoxdextrigoxparaxtrêsxtigresxtristes'
+print(TEXTO.replace('x', ' '))
+
+Num0 = input('Digite primeiro numero')
+Num1 = input('Digite segundo numero')
+Soma = Num0 + Num1
+Sobra = Num0 + Num1
+
+print(f'Soma: {Soma}')
+print(f'Sobra: {Sobra}')
