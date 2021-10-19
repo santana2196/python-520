6f94c61783285892a6d112e913ea88ac2fb7db5e
Santana2196 <Santana2196@mail.com>
2021-10-19 19:25:02 -0300

    primeiro commit

--- aula-1/aula1.py
+++ aula-1/aula1.py
@@ -15,7 +15,7 @@ print(TEXTO.replace('x', ' '))
 Num0 = input('Digite primeiro numero')
 Num1 = input('Digite segundo numero')
 Soma = Num0 + Num1
-Sobra = Num0 + Num1
+Sobra = Num0 - Num1
 
 print(f'Soma: {Soma}')
 print(f'Sobra: {Sobra}')
