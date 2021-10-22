c24f6e33f85af7a64783219f01854491c8524f70
Santana2196 <Santana2196@mail.com>
2021-10-22 18:55:54 -0300

    funcoes anonima com dicionario

--- aula-1/atividade_quitanda/anoni_dicio.py
+++ aula-1/atividade_quitanda/anoni_dicio.py
@@ -6,10 +6,10 @@ operacoes = {
 }
 
 while True:
-    valor1 = float(input('Digite a operação: '))
-    valor2 = float(input('Digite a operação: '))
+    valor1 = int(input('Digite primeiro valor: '))
+    valor2 = int(input('Digite segundo valor: '))
 
-    print(operacoes.get('soma')(valor1,valor2))
-    print(operacoes.get('sub')(valor1,valor2))
-    print(operacoes.get('mult')(valor1,valor2))
-    print(operacoes.get('div')(valor1,valor2))
+    print(f"A soma do {valor1} + {valor2} = {operacoes.get('soma')(valor1,valor2)}")
+    print(f"A subtracao do {valor1} - {valor2} = {operacoes.get('sub')(valor1,valor2)}")
+    print(f"A multiplicação do {valor1} * {valor2} = {operacoes.get('mult')(valor1,valor2)}")
+    print(f"A divisao do {valor1} / {valor2} = {operacoes.get('div')(valor1,valor2)}", '\n')
